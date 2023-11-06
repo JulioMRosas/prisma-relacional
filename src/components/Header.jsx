b1b1423b6c99f911f7719bf7df5ab1104efd0d41
JulioMRosas <al21760482@ite.edu.mx>
2023-11-06 08:45:22 -0800

    se añadieron rutas al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,9 +12,45 @@ const Header = () => {
     <>
       {/* Normal menu */}
       <header className='hidden sm:grid grid-cols-3 p-6 bg-[#DDDDDD] text-[#333]'>
-          <Link href={"../"} className='justify-self-center'>Inicio</Link>
-          <Link href={"../register"} className='justify-self-center'>Registrar</Link>
-          <Link href={"../consult"} className='justify-self-center'>Ver Registros</Link>
+        <Link href={"../"} className='justify-self-center'>Inicio</Link>
+        <Popover>
+          <Popover.Button>Registrar</Popover.Button>
+          <Transition
+          enter="transition-opacity duration-75"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity duration-150"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          >
+          <Popover.Overlay className="fixed inset-0 bg-black opacity-30" />
+          <Popover.Panel className='absolute text-[#333] bg-[#DDDDDD] p-6 rounded-md w-2/4 border border-[#333]'>
+            <ul className='flex flex-col space-y-5'>
+              <Link href={"../registeruser"}>Usuarios</Link>
+              <Link href={"../registerproduct"}>Productos</Link>
+            </ul>
+          </Popover.Panel>
+        </Transition>
+        </Popover>
+        <Popover>
+          <Popover.Button>Ver Registros</Popover.Button>
+          <Transition
+          enter="transition-opacity duration-75"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity duration-150"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          >
+          <Popover.Overlay className="fixed inset-0 bg-black opacity-30" />
+          <Popover.Panel className='absolute text-[#333] bg-[#DDDDDD] p-6 rounded-md w-2/4 border border-[#333]'>
+            <ul className='flex flex-col space-y-5'>
+              <Link href={"../userslist"}>Usuarios</Link>
+              <Link href={"../productslist"}>Productos</Link>
+            </ul>
+          </Popover.Panel>
+        </Transition>
+        </Popover>
       </header>
 
       {/* Responsive menu */}
